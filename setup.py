2463a17b2985a280ac59fee16bfd8430db4d1dd3
MaxMasterCOder <126357979+MaxMasterCOder@users.noreply.github.com>
2023-02-25 21:16:40 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MaxMasterCoder",
+    version="0.0.1",
+    author="Max Rodriguez Guell",
+    author_email="rodriguezguell.max@gmail.com",
+    description="An useful package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaxMasterCOder/AppMaker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
